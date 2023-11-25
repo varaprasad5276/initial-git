6d321a3e5769c22fc36bcdcae1e38a8012b26319
vara prasad <varaprasad@varas-MacBook-Air.local>
2023-11-25 22:34:51 +0530

    new files added

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let b=document.getElementById("but");
+let body=document.getElementById("b");
+
+
+  //b.onclick=change;
+  setInterval((
+  function change()
+  {
+
+  let v='012ABCEDF';
+  let c='#';
+  for(let j=1;j<=6;j++)
+  {
+    let g=Math.ceil(Math.random()*8);
+    c=c+v.charAt(g);
+  }
+  console.log(c);
+    body.style.background=c;
+    console.log(i);
+
+  }),2000)
+
