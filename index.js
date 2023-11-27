d15111f4f7650583766c90d6e30b06dbeae414a9
vara prasad <varaprasad@varas-MacBook-Air.local>
2023-11-27 22:04:01 +0530

    new updates for bubblibg,capturing & for in, for of loop

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 
-let b=document.getElementById("but");
-let body=document.getElementById("b");
+// let b=document.getElementById("but");
+// let body=document.getElementById("b");
+// let t=document.getElementById("t");
 
 
-  b.onclick=change;
+//   b.onclick=change;
 
-  function change()
-  {
+//   function change()
+//   {
 
-  let v='012ABCEDF';
-  let c='#';
-  for(let j=1;j<=6;j++)
-  {
-    let g=Math.ceil(Math.random()*8);
-    c=c+v.charAt(g);
-  }
-  console.log(c);
-    body.style.background=c;
-    console.log(i);
+//   let v='012ABCEDF';
+//   let c='#';
+//   for(let j=1;j<=6;j++)
+//   {
+//     let g=Math.ceil(Math.random()*8);
+//     c=c+v.charAt(g);
+//   }
+//   console.log(c);
+//     body.style.background=c;
+// t.hidden=true;
+// //   }
+// document.querySelector("#grandparent")
+// .addEventListener('click',()=>
+// {
+// console.log("Grand Parent Clicked");
+// });//bubbling
 
-  }
+// document.querySelector("#parent")
+// .addEventListener('click',(e)=>
+// {
+//   e.stopPropagation();
+// console.log("Parent Clicked");
+// });//bubbbling
 
+// document.querySelector("#child")
+// .addEventListener('click',()=>
+// {
+// console.log("Child Clicked");
+// },true);//capturing
+let arr=['a','b','c','d','e'];
+arr.object=10;
+console.log("for...in Loop")
+console.log(arr.length)
+for( let value in arr)
+{
+    console.log(value+"  "+arr[value]);
+
+}
+console.log("for...of Loop")
+
+for(let value of arr)
+{
+    console.log(value+"  "+arr[value]);
+
+}
+console.log(arr.length)
